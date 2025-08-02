4fdbbffa37f399a8b93937e79aca90e7540b7843
Kevin Smith <vndinmchine@gmail.com>
2025-08-02 00:12:12 -0400

    Update rescan_movies.py

--- rescan_movies.py
+++ rescan_movies.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
 if __name__ == "__main__":
     logging.info("Rescanning movies...")
     init_db()
-    movies = scan_movies("/mnt/Movies")
+    movies = scan_movies()
     for movie in movies:
         add_movie_to_db(movie)
     logging.info(f"✅ Rescan complete. {len(movies)} folders checked.")
